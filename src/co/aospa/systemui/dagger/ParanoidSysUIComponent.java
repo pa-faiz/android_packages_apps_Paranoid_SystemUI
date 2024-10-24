303278691ebadcf34a6b5067305404cfdf993e11
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-24 11:39:07 +0900

    ParanoidSystemUI: Match with AOSP V reference modules
    Change-Id: I73eb50a40bc52c803b412634e1b2bc8c0b1de69f
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/co/aospa/systemui/dagger/ParanoidSysUIComponent.java
+++ src/co/aospa/systemui/dagger/ParanoidSysUIComponent.java
@@ -24,10 +24,10 @@ import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.SystemUIBinder;
 import com.android.systemui.dagger.SystemUICoreStartableModule;
 import com.android.systemui.dagger.SystemUIModule;
-import com.android.systemui.globalactions.ShutdownUiModule;
 import com.android.systemui.keyguard.CustomizationProvider;
 import com.android.systemui.statusbar.NotificationInsetsModule;
 import com.android.systemui.statusbar.QsFrameTranslateModule;
+import com.android.systemui.unfold.SysUIUnfoldModule;
 
 import dagger.Subcomponent;
 
@@ -40,7 +40,6 @@ import dagger.Subcomponent;
         DependencyProvider.class,
         NotificationInsetsModule.class,
         QsFrameTranslateModule.class,
-        ShutdownUiModule.class,
         SystemUIBinder.class,
         SystemUIModule.class,
         SystemUICoreStartableModule.class,
