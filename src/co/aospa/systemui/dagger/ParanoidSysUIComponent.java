fed8b3047ba458811e2da5699c10b05a742851e9
Omkar Chandorkar <gotenksIN@aospa.co>
2024-10-24 11:39:52 +0900

    ParanoidSystemUI: Update to V
    - Sync VolumeModule with SystemUI
    - Remove ParanoidSystemUICoreStartableModule as VolumeUI can be overriden in ParanoidVolumeModule again
    
    Change-Id: I9d2f311e1c778442e679c454c919c0151d6a5122
    Signed-off-by: Omkar Chandorkar <gotenksIN@aospa.co>

--- src/co/aospa/systemui/dagger/ParanoidSysUIComponent.java
+++ src/co/aospa/systemui/dagger/ParanoidSysUIComponent.java
@@ -22,6 +22,7 @@ import com.android.systemui.dagger.ReferenceSysUIComponent;
 import com.android.systemui.dagger.SysUIComponent;
 import com.android.systemui.dagger.SysUISingleton;
 import com.android.systemui.dagger.SystemUIBinder;
+import com.android.systemui.dagger.SystemUICoreStartableModule;
 import com.android.systemui.dagger.SystemUIModule;
 import com.android.systemui.globalactions.ShutdownUiModule;
 import com.android.systemui.keyguard.CustomizationProvider;
@@ -42,7 +43,7 @@ import dagger.Subcomponent;
         ShutdownUiModule.class,
         SystemUIBinder.class,
         SystemUIModule.class,
-        ParanoidSystemUICoreStartableModule.class,
+        SystemUICoreStartableModule.class,
         ParanoidSystemUIModule.class})
 public interface ParanoidSysUIComponent extends SysUIComponent {
     @SysUISingleton
