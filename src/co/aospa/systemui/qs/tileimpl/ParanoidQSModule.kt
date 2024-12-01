685538a9aa4390af2498a7341923e565323a3e3c
Pranav Vashi <neobuddy89@gmail.com>
2024-12-01 22:32:48 +0530

    ParanoidSystemUI: Add Weather tile based on OmniJaws client
    Change-Id: I6a735474d01f3bf0363530172a6472038a4a7d8e
    Signed-off-by: Fazil Sheik <fazil.sheik96@gmail.com>

--- src/co/aospa/systemui/qs/tileimpl/ParanoidQSModule.kt
+++ src/co/aospa/systemui/qs/tileimpl/ParanoidQSModule.kt
@@ -26,6 +26,7 @@ import co.aospa.systemui.qs.tiles.HeadsUpTile
 import co.aospa.systemui.qs.tiles.PowerShareTile
 import co.aospa.systemui.qs.tiles.UsbTetherTile
 import co.aospa.systemui.qs.tiles.WifiTile
+import co.aospa.systemui.qs.tiles.WeatherTile
 import com.android.systemui.qs.tileimpl.QSTileImpl
 import dagger.Binds
 import dagger.Module
@@ -94,5 +95,11 @@ interface ParanoidQSModule {
     @IntoMap
     @StringKey(FivegTile.TILE_SPEC)
     fun bindFivegTile(fivegTile: FivegTile): QSTileImpl<*>
+
+    /** Inject WeatherTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(WeatherTile.TILE_SPEC)
+    fun bindWeatherTile(weatherTile: WeatherTile): QSTileImpl<*>
 
 }
